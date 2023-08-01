0f4a6daaef6cb02c604bdd3ead508ecc9e2bd24d
CrimsonCoder42 <nostro37@gmail.com>
2023-08-01 09:37:30 -0400

    main added

---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+
+
+URL = 'https://www.linkedin.com/jobs/search/?currentJobId=3637630487&distance=10&f_AL=true&f_E=2%2C3%2C4&f_JT=F&f_SB2=4&geoId=102571732&keywords=software%20engineer&location=New%20York%2C%20New%20York%2C%20United%20States&refresh=true&sortBy=R'
+
+
